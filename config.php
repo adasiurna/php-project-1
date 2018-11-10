a3c8fb77f1ac96989fb0340bb514b847cb946f57
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2018-11-11 01:54:58 +0200

    pridedu config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+const ADMIN_PASSWORD_HASH = '$2y$10$.GbzOC3tvxCqV7OM8NZMXevK3H0OMVowiNphSHmR6nCpm5EqlO30y';
+const BASE_URL = 'http://localhost/php-project-1';
+const FILE_FOLDER = 'files';
+const IMAGE_FILE_EXTENSIONS = ['png', 'jpeg', 'jpg'];
+const MAX_FILE_SIZE = 524288; //0.5mb
+const ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'php', 'txt', 'pdf', 'docx'];
